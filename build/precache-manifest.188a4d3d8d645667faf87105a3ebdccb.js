3e5c81e7cee9dd1584ae847da3f923974aad7f10
leon <leonl2@bitbucket.org>
2019-12-05 16:02:50 -0500

    add build

---
+++ build/precache-manifest.188a4d3d8d645667faf87105a3ebdccb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e7bcb2f6a27893e15c954ab74f1360",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19e6f83ce31b8bb5864b",
+    "url": "/static/css/main.3a69bc9a.chunk.css"
+  },
+  {
+    "revision": "9e85a1ab67f34dd7e681",
+    "url": "/static/js/2.7211df77.chunk.js"
+  },
+  {
+    "revision": "19e6f83ce31b8bb5864b",
+    "url": "/static/js/main.4bef5a8d.chunk.js"
+  },
+  {
+    "revision": "68044b3ca619fe1bb1ff",
+    "url": "/static/js/runtime-main.80f93acb.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "3c15d06099ae3b0f2a35314b39109e2c",
+    "url": "/static/media/pokemon_wallpaper.3c15d060.jpg"
+  }
+]);
