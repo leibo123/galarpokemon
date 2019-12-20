eb92e8405161ea26b24522bb11543c0376b18318
leon <leonl2@bitbucket.org>
2019-12-20 00:56:58 -0500

    add all remaining pokemon images

---
+++ build/precache-manifest.0f0f39bc3b8d6d92392720ed80d141d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0eb2ff0a9ddb5368bdcf300bdae3586",
+    "url": "/galarpokemon/index.html"
+  },
+  {
+    "revision": "2b1a6dc589d5e3058e16",
+    "url": "/galarpokemon/static/css/main.864bc128.chunk.css"
+  },
+  {
+    "revision": "9e85a1ab67f34dd7e681",
+    "url": "/galarpokemon/static/js/2.7211df77.chunk.js"
+  },
+  {
+    "revision": "2b1a6dc589d5e3058e16",
+    "url": "/galarpokemon/static/js/main.8b21c2d5.chunk.js"
+  },
+  {
+    "revision": "454b7f1fc50af3b4f518",
+    "url": "/galarpokemon/static/js/runtime-main.58ce681f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/galarpokemon/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "3c15d06099ae3b0f2a35314b39109e2c",
+    "url": "/galarpokemon/static/media/pokemon_wallpaper.3c15d060.jpg"
+  }
+]);
