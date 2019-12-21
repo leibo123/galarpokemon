7c6614ab1098d80083c0ef474ad9ebd4305d2a5e
leon <leonl2@bitbucket.org>
2019-12-21 12:48:11 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/galarpokemon/precache-manifest.70401376d96bb928c1ef5ca3cc96e91e.js"
+  "/galarpokemon/precache-manifest.b23588d6a25763bb2fcbbb105926efd2.js"
 );
 
 self.addEventListener('message', (event) => {
